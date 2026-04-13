5481b2346712bb72ed96b8e12d1f89794b171689
Jon Gabriel <jgmoegster@protonmail.com>
2026-04-13 14:18:36 +0200

    Endret koblingspunkter til drive for motorene

--- include/Drive.hpp
+++ include/Drive.hpp
@@ -4,13 +4,13 @@
 
 class Motordriver {
 private:
-    int AIN1_ = 10;
-    int AIN2_ = 11;
-    int PWMA_ = 9;
+    int AIN1_ = 27;
+    int AIN2_ = 14;
+    int PWMA_ = 26;
 
-    int BIN1_ = 30;
+    int BIN1_ = 12;
     int BIN2_ = 21;
-    int PWMB_ = 15;
+    int PWMB_ = 13;
 
 public:
     Motordriver();
